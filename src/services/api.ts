e46cd4250fa4dc9e5bf1512aa53cd9aabf04ce27
REDDIRANI <rayadurgamreddirani1@gmail.com>
2025-05-01 22:57:48 +0530

    modified base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://b2b-marketplace-backend-1.onrender.com',
 });
 
 export default api;
